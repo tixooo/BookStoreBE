acb5b1c46830fb21dd2c0aacb4cad4071a826a8d
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-24 18:51:44 +0200

    started working on the bookstore boilerplate

---
+++ src/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.DB_CONNECTION)
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (err) {
+        console.error(`Error: ${err.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
