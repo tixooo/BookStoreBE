38bf97b70cbc5b42dd8e74fea4a61e91673edd6c
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-24 19:22:27 +0200

    added user schema

---
+++ models/User.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    username: {type: String, required: true, unique: true},
+    password: {type: String, required: true},
+    email: {type: String, required: true, unique: true},
+    image: {type: String, required: true},
+    fullName: {type: String, required: true},
+})
+
+const User = mongoose.model('User', userSchema, 'BookstoreUser')
+
+export default User
