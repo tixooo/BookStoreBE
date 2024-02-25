e3c2f2161d3d266a64ef5a65bbca16199fc70ee3
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-25 22:54:09 +0200

    cleaned the code in main

--- src/main.js
+++ src/main.js
@@ -7,16 +7,10 @@ import auth from '../routes/auth.js';
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors(corsOptions))
-app.use((req,res,next) => {
-    if (req.url.endsWith('.css')) {
-        res.setHeader('Content-Type', 'text/css');
-    }
-    next();
-})
 app.use('/public', express.static('public'));
-// app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json())
+app.use(cors(corsOptions))
+
 connectDB().then(() => {
     console.log('Connected to MongoDB');
     app.listen(port, "0.0.0.0", () => {
