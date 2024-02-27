2b489f173e06089d9a4336205693dc5d8a445c30
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-27 18:55:45 +0200

    added new IP

--- routes/cors.js
+++ routes/cors.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
     "http://localhost:3000",
     "http://localhost:5500",
-    "https://127.0.0.1:5500"
+    "http://127.0.0.1:5500",
+    "http://127.0.0.1:5500/"
 ];
 
 const corsOptions = {
