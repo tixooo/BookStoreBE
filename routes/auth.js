51c57ac9732833110c07d8a39f33bcac792f0ab3
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-28 18:38:10 +0200

    added login logic

--- routes/auth.js
+++ routes/auth.js
@@ -35,4 +35,28 @@ router.post('/register', async (req, res) => {
 
 })
 
+router.post('/login', async (req, res) => {
+    const {username, password} = req.body;
+    try {
+        const user = await User.findOne({username});
+        if(!user) {
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) {
+            return res.status(400).json({message: 'Invalid credentials'});
+        }
+
+        const userData = {
+            username: user.username,
+            email: user.email,
+        }
+        res.status(200).json({message: 'Login successful', user:  userData})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'Server error'})
+    }
+})
+
 export default router
