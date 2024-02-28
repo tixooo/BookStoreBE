3213ff29e19713ae568cf472c4e578e6f3cbf43b
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-28 18:28:01 +0200

    changed cors

--- routes/cors.js
+++ routes/cors.js
@@ -1,8 +1,9 @@
 const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://127.0.0.1:5500/"
+    "*"
+    // "http://localhost:3000",
+    // "http://localhost:5500",
+    // "http://127.0.0.1:5500",
+    // "http://127.0.0.1:5500/"
 ];
 
 const corsOptions = {
