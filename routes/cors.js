3815f74deb0e4184ff4e4dff0a00a15b50e8f3a4
Konstantin <konstantin.stefanov87@gmail.com>
2024-02-29 18:31:49 +0200

    added the bookstore url

--- routes/cors.js
+++ routes/cors.js
@@ -1,9 +1,5 @@
 const allowedOrigins = [
-    "*"
-    // "http://localhost:3000",
-    // "http://localhost:5500",
-    // "http://127.0.0.1:5500",
-    // "http://127.0.0.1:5500/"
+    "https://dimitarbh.github.io/Bookstore/"
 ];
 
 const corsOptions = {
