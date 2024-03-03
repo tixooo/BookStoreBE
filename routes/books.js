14f159ba0da806bcebc0205a2c5b488c66a08f8c
Konstantin <konstantin.stefanov87@gmail.com>
2024-03-03 15:57:48 +0200

    added searching by book ID and added post route

--- routes/books.js
+++ routes/books.js
@@ -13,4 +13,34 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id)
+        res.json(book)
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'Server error'})
+    }
+})
+
+router.post('/', async (req, res) => {
+    const {title, author, description, publisher, image, price, discountedPrice} = req.body;
+    try {
+        const newBook = new Book({
+            title,
+            author,
+            description,
+            publisher,
+            image,
+            price,
+            discountedPrice
+        })
+        await newBook.save()
+        res.status(201).json({message: 'Book created successfully'})
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({message: 'Server error'})
+    }
+})
+
 export default router
